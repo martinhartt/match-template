9030e68208adb60129682dd857dd3b006721dfdd
Martin Kubat <martinkubat@icloud.com>
2016-04-23 15:48:11 +0100

    Fixed indentation to two spaces

--- index.js
+++ index.js
@@ -3,18 +3,18 @@
 var _ = require('underscore');
 
 module.exports = function matchTemplate(object, template) {
-    var cleanTemplate = _.omit(template, function (i) {
-        return !i;
-    });
-    var cleanKeys = _.keys(cleanTemplate);
-    var result = {};
-    cleanKeys.forEach(function (k) {
-        if (_.isObject(template[k])) {
-            result[k] = matchTemplate(object[k], template[k]);
-        } else {
-            result[k] = object[k];
-        }
-    });
+  var cleanTemplate = _.omit(template, function (i) {
+    return !i;
+  });
+  var cleanKeys = _.keys(cleanTemplate);
+  var result = {};
+  cleanKeys.forEach(function (k) {
+    if (_.isObject(template[k])) {
+      result[k] = matchTemplate(object[k], template[k]);
+    } else {
+      result[k] = object[k];
+    }
+  });
 
-    return result;
+  return result;
 };
